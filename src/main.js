969e017b8445c1a4096f085987a702002a7b56cb
Aarav Nair <224329735+Coding-AN@users.noreply.github.com>
2026-09-23 18:05:14 -0400

    Modification: Added the API Call for the picture, and added the display for the title, image, and explanation

--- src/main.js
+++ src/main.js
@@ -1,5 +1,29 @@
-document.querySelector('#app').innerHTML = `
-  <div>
-  <h>Hello World!</h>
-  </div>
-`
+const API_KEY = import.meta.env.VITE_NASA_API_KEY;
+fetch(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}`)
+.then(response => response.json())
+.then(data => {
+  console.log(data);;
+  let media;
+  if(data.media_type === "image")
+  {
+    media = `<img src="${data.url}">`
+  }
+  else if(data.url.includes("youtube"))
+  {
+    media = `<iframe src="${data.url}">`
+  }
+  else
+  {
+    media = `<video src="${data.url}">`
+  }
+
+  document.querySelector('#app').innerHTML = `
+    <div>
+    <h1>${data.title}</h1>
+    ${media}
+    <p>${data.explanation}</p>
+    </div>
+  `})
+  .catch(err => 
+    document.querySelector("#app").innerHTML = `<p>Error: ${err.message}</p>`
+);
